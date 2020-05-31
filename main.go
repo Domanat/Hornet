d7ec73bf5714a77d324980bf10fe205c93fb16f1
Domanat <maxim.tomilko@yandex.ru>
2020-05-31 16:16:50 +0300

    Add password to the db to the config file

--- main.go
+++ main.go
@@ -3,18 +3,26 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 
-	var err error
-	db, err = sql.Open("mysql", "root:Qwerty0106@tcp(127.0.0.1:3306)/hornet")
+	password, err := ioutil.ReadFile("dbConfig.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	dataSourceName := fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/hornet", password)
+
+	db, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
 
 	http.HandleFunc("/", logup)
+	http.HandleFunc("/thanks", thanks)
 	http.HandleFunc("/login", login)
 
 	fmt.Println("Listening...")
